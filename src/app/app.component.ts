918a6959f984703ac1e8e96adb867957ace1fbd8
Bradon Fredrickson <bfredrickson@healthequity.com>
2018-11-07 13:39:38 -0700

    Ability to edit a coffee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,20 +16,42 @@ import { CoffeeService } from './coffee.service';
 export class AppComponent implements OnInit {
   title = 'CoffeeTracker';
   coffeeCollection: AngularFirestoreCollection<Coffee>;
+  coffeeDoc: AngularFirestoreDocument<Coffee>;
   coffees: Observable<Coffee[]>;
-  snapshot: any;
+  coffeeDesc: string;
+  coffeeDate: string;
+  editMode = false;
+  coffeeToEdit: any = {};
 
   constructor(private db: AngularFirestore, private coffeeService: CoffeeService) {}
 
   ngOnInit() {
+
+    // Initialize the list of Coffees
     this.coffeeCollection = this.db.collection(config.collection_endpoint);
 
     this.coffees = this.coffeeCollection.snapshotChanges()
     .pipe(map(actions => actions.map(a => {
       const data = a.payload.doc.data() as Coffee;
-      // console.log(data);
       const id = a.payload.doc.id;
+      console.log(a.payload);
+      console.log({id, ...data });
       return { id, ...data };
     })));
+  }
+
+  editCoffee(coffee) {
+    console.log(coffee);
+
+    // Set coffeeToEdit and editMode
+    this.coffeeToEdit = coffee;
+    this.editMode = true;
+
+    // Set form value
+    coffee.description = this.coffeeDesc;
+    coffee.date = this.coffeeDate;
+
+    const coffeeId = this.coffeeToEdit.id;
+    this.coffeeService.updateCoffee(coffeeId, this.coffeeToEdit);
   }
 }
